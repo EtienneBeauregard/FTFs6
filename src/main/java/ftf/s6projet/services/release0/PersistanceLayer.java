0b2c53e9fd29683622b0af873e7bee9cf3405ada
François Poulin <francois13p@gmail.com>
2018-06-01 14:09:20 -0400

    TG-1 TG-44 TG-92 Release 0 Persistance Layer

---
+++ src/main/java/ftf/s6projet/services/release0/PersistanceLayer.java
@@ -0,0 +1,25 @@
+/**************************************************************************************************************
+ * Classe implémentant la couche de persistance du micro service du release 0.
+ * 
+ * @author  Francois Poulin
+ * @date    31 mai 2018
+ * @version 1.0
+ * 
+ *************************************************************************************************************/
+
+// Packages:
+package ftf.s6projet.services.release0;
+
+// Imports généraux:
+import org.json.*;
+import java.io.*;
+
+public class PersistanceLayer {
+    public String getPassword(String p_name){
+        File file = new File("D:\\ProjetS6\\FTFS6\\release0.json");
+        String password = "";
+        
+        return password;
+    }
+    
+}
