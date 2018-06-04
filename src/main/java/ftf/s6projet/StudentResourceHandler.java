94ac6e4605a1fa2d47cb0dd235a3e5f84f3e5f55
Éric Poirier <eric.poirier7@usherbrooke.ca>
2018-06-03 20:56:22 -0400

    TG-1 TG-44 Tous les tests sont terminés et passent. Tous les fichiers sont documentés convenablement. Ce projet serait prêt à être présenté en démo, au besoin.

--- src/main/java/ftf/s6projet/StudentResourceHandler.java
+++ src/main/java/ftf/s6projet/StudentResourceHandler.java
@@ -1,11 +1,13 @@
 /**********************************************************************************************
+ * Fichier pour handler d'appel à l'API REST concernant la ressource "students".
+ * 
+ * @author Éric Poirier
+ * @date   Juin 2018.
  * 
  **********************************************************************************************/
 
 package ftf.s6projet;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 import javax.ws.rs.Produces;
@@ -17,22 +19,19 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 /**********************************************************************************************
- * 
- * 
- * @author Éric Poirier
- * @date Juin 2018
+ * Classe permettant la gestion des ressources "student" lors d'appels à l'API REST du
+ * microservice de test. Les requêtes Http GET, POST, PUT et DELETE sont supportées.
  * 
  **********************************************************************************************/
 @Path("students")
 public class StudentResourceHandler {
 
     /******************************************************************************************
+     * Prépare la liste complète des étudiants enregistrés dans la base de donnée.
      * 
-     * 
-     * @return
+     * @return La liste des étudiants (JSON).
      * 
      ******************************************************************************************/
     @GET
@@ -41,11 +40,13 @@ public class StudentResourceHandler {
         return DataBase.theDataBase().getStudents();
     }
 
-    
+
     /******************************************************************************************
+     * Prépare un étudiant enregistré dans la base de donnée. L'absence de l'étudiant demandé
+     * n'est pas gérée pour le moment.
      * 
-     * 
-     * @return
+     * @param   p_cip Le CIP de l'étudiant voulu.
+     * @return  L'étudiant voulu (JSON).
      * 
      ******************************************************************************************/
     @GET
@@ -57,46 +58,51 @@ public class StudentResourceHandler {
 
 
     /******************************************************************************************
+     * Ajoute un étudiant à la base de données. Les étudiants dupliqués ne sont pas gérés pour
+     * le moment.
      * 
-     * 
-     * @return
+     * @param   p_newStudent L'étudiant à ajouter.
+     * @return  L'étudiant ajouté (JSON).
      * 
      ******************************************************************************************/
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public StudentResource onNewStudent(StudentResource p_newStudent) throws URISyntaxException {
+    public StudentResource onNewStudent(StudentResource p_newStudent) {
         DataBase.theDataBase().addStudent(p_newStudent);
         return DataBase.theDataBase().getStudent(p_newStudent.getCip());
     }
-    
-    
+
+
     /******************************************************************************************
+     * Mets à jour le nom d'un étudiant dans la base de données. On utilise les CIP afin
+     * de trouver le bon étudiant dans la base de données. Le cas où l'étudiant n'exite pas
+     * dans la base de données n'est pas traité pour le moment.
      * 
-     * 
-     * @return
+     * @param  p_updatedStudent L'étudiant à jour (même CIP que l'ancien, mais un 
+     *                          nom différent).
+     * @return L'étudiant mis à jour (JSON).
      * 
      ******************************************************************************************/
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public StudentResource onModifyExistingStudent(StudentResource p_updatedStudent) {
-        // Find the actual student thats needs to be updated:
+        // Trouver l'étudiant qu'on veut mettre à jour:
         String cip = p_updatedStudent.getCip();
         StudentResource oldStudent = DataBase.theDataBase().getStudent(cip);
-        
-        // Update his name:
+
+        // Mettre à jour son nom:
         DataBase.theDataBase().modifyStudentName(oldStudent, p_updatedStudent.getName());;
-        
-        // Return the appropriate response:
+
         return DataBase.theDataBase().getStudent(cip);
     }
-    
-    
+
+
     /******************************************************************************************
+     * Retire un étudiant de la base de données. Si l'étudiant n'existe pas, rien ne se passe.
      * 
-     * 
-     * @return
+     * @param p_cip Le CIP de l'étudiant à retirer de la base de données.
      * 
      ******************************************************************************************/
     @DELETE
--- src/test/java/ftf/s6projet/integration/StudentResourceHandlerTest.java
+++ src/test/java/ftf/s6projet/integration/StudentResourceHandlerTest.java
@@ -9,40 +9,104 @@ package ftf.s6projet.integration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ftf.s6projet.DataBase;
+import ftf.s6projet.StudentResource;
+import ftf.s6projet.StudentResourceHandler;
+
+/**********************************************************************************************
+ * Stratégie pour ces tests: il y a une librairie permettant de tester des méthodes implantées
+ * pour Jersey, mais je ne l'utilise pas ici. Je me borne à tester les effets de ces méthodes
+ * sur la base de données en RAM ainsi que les sorties, lorsque nécessaire. Toutefois, pour la
+ * prochaine itération, les tests devront être plus large.
+ * 
+ *
+ **********************************************************************************************/
 class StudentResourceHandlerTest {
 
-    /*
+    /******************************************************************************************
+     * Méthode d'initialisation de tests. Comme nous testons un singleton, on doit le
+     * le réinitialiser à chaque test pour s'assurer que les tests ne s'affectent pas les
+     * uns les autres.
+     * 
+     ******************************************************************************************/
     @BeforeEach
-    void setUp() throws Exception {
+    void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        // On retrouve le membre qui contient l'instance du singleton:
+        Field singletonInstance = DataBase.class.getDeclaredField("INSTANCE");
+
+        // On s'assure que ce membre est accessible, même s'il est privé:
+        singletonInstance.setAccessible(true);
+
+        // On le déinitialise pour annuler les changements du dernier test:
+        singletonInstance.set(null, null);
     }
+
 
     @Test
     void testOnGetAllStudents() {
-        fail("Not yet implemented");
+        ArrayList<StudentResource> studentList = new ArrayList<StudentResource>();
+        studentList = DataBase.theDataBase().getStudents();
+        
+        StudentResourceHandler handler = new StudentResourceHandler();
+        assertTrue(studentList.equals(handler.onGetAllStudents()));
     }
+
 
     @Test
     void testOnGetOneStudent() {
-        fail("Not yet implemented");
+        StudentResource student = new StudentResource("Éric", "poie2104");
+
+        StudentResourceHandler handler = new StudentResourceHandler();
+        assertTrue(student.equals(handler.onGetOneStudent(student.getCip())));
     }
+
 
     @Test
     void testOnNewStudent() {
-        fail("Not yet implemented");
+        StudentResource newStudent = new StudentResource("Bob", "grat9999");
+
+        // On s'assure d'abord que le nouvel étudiant n'est pas déjà dans la base de données:
+        assertFalse(DataBase.theDataBase().getStudents().contains(newStudent));
+
+        // Puis on l'ajoute:
+        StudentResourceHandler handler = new StudentResourceHandler();
+        handler.onNewStudent(newStudent);
+
+        // Ici je crée un nouvel objet identique au premier pour m'assurer que ce ne sont
+        // pas les références qui sont comparés mais bien les membres:
+        StudentResource newStudent2 = new StudentResource("Bob", "grat9999");
+
+        // Puis on conclue le test:
+        assertTrue(DataBase.theDataBase().getStudents().contains(newStudent2));
     }
+
 
     @Test
     void testOnModifyExistingStudent() {
-        fail("Not yet implemented");
+        StudentResource oldStudent = new StudentResource("Éric", "poie2104");
+        StudentResource newStudent = new StudentResource("Bob", "poie2104");
+        assertTrue(DataBase.theDataBase().getStudents().contains(oldStudent));
+        assertFalse(DataBase.theDataBase().getStudents().contains(newStudent));
+
+        StudentResourceHandler handler = new StudentResourceHandler();
+        handler.onModifyExistingStudent(newStudent);
+        assertTrue(DataBase.theDataBase().getStudents().contains(newStudent));
     }
+
 
     @Test
     void testOnRemoveStudent() {
-        fail("Not yet implemented");
-    }
-    */
+        StudentResource studentToRemove = new StudentResource("Éric", "poie2104");
+        assertTrue(DataBase.theDataBase().getStudents().contains(studentToRemove));
 
+        StudentResourceHandler handler = new StudentResourceHandler();
+        handler.onRemoveStudent(studentToRemove.getCip());
+        assertFalse(DataBase.theDataBase().getStudents().contains(studentToRemove));
+    }
 }
