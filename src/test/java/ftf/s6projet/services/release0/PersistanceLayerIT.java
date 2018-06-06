10aa504ad15c5c7e41591286aa5d79993b5688cc
François Poulin <francois13p@gmail.com>
2018-06-06 14:31:11 -0400

    TG-1 TG-44 TG-92 PersistanceLayer done and tested, ready for implementation.  Doc is missing.

--- src/test/java/ftf/s6projet/services/release0/PersistanceLayerIT.java
+++ src/test/java/ftf/s6projet/services/release0/PersistanceLayerIT.java
@@ -2,10 +2,14 @@ package ftf.s6projet.services.release0;
 
 
 import ftf.s6projet.services.release0.PersistanceLayer;
+import ftf.s6projet.services.release0.Student;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,44 +17,73 @@ public class PersistanceLayerIT {
     @BeforeEach
     void setUpTests() {
         PersistanceLayer persistanceLayer = new PersistanceLayer();
-        if (persistanceLayer.getUser("test1234") != null) {
-            persistanceLayer.delUser("test1234");
+        Student testStudent = new Student("test", "test1234", "1234");
+        if (persistanceLayer.getUser(testStudent) != null) {
+            persistanceLayer.delUser(testStudent);
+        }
+    }
+
+    @AfterAll
+    static void cleanUpTest() {
+        PersistanceLayer persistanceLayer = new PersistanceLayer();
+        Student testStudent = new Student("test", "test1234", "1234");
+        if (persistanceLayer.getUser(testStudent) != null) {
+            persistanceLayer.delUser(testStudent);
         }
     }
 
     @Test
     void getUserFromJsonFile() {
         PersistanceLayer persistanceLayer = new PersistanceLayer();
-        assertEquals(persistanceLayer.getUser("pouf1234")[2], "1234");
+        Student testStudent = new Student("test", "test1234", "1234");
+        persistanceLayer.postUser(testStudent);
+        assertEquals(persistanceLayer.getUser(testStudent).m_cip, testStudent.m_cip);
+        assertEquals(persistanceLayer.getUser(testStudent).m_name, testStudent.m_name);
+        assertEquals(persistanceLayer.getUser(testStudent).m_password, testStudent.m_password);
+    }
+    
+    @Test
+    void getAllUsersFromJsonFile() {
+        PersistanceLayer persistanceLayer = new PersistanceLayer();
+        ArrayList<Student> studentsList = persistanceLayer.getAllUsers();
+        assertEquals(8, studentsList.size());
     }
 
     @Test
-    void postUserFromJsonFile() {
+    void postUserInJsonFile() {
         PersistanceLayer persistanceLayer = new PersistanceLayer();
-        persistanceLayer.postUser("test1234", "t_fname", "t_lname", "t_password");
-        String[] user = persistanceLayer.getUser("test1234");
-        assertEquals(user[0], "t_fname");
-        assertEquals(user[1], "t_lname");
-        assertEquals(user[2], "t_password");
-    }
+        Student testStudent = new Student("test", "test1234", "1234");
 
+        persistanceLayer.postUser(testStudent);
+        Student user = persistanceLayer.getUser(testStudent);
+        assertEquals(user.m_cip, "test1234");
+        assertEquals(user.m_name, "test");
+        assertEquals(user.m_password, "1234");
+    }
+    
     @Test
     void putUserFromJsonFile() {
         PersistanceLayer persistanceLayer = new PersistanceLayer();
-        persistanceLayer.postUser("test1234", "t_fname", "t_lname", "t_password");
-        persistanceLayer.putUser("test1234", "put_fname", "put_lname", "put_password");
-        String[] user = persistanceLayer.getUser("test1234");
-        assertEquals(user[0], "put_fname");
-        assertEquals(user[1], "put_lname");
-        assertEquals(user[2], "put_password");
+        Student testStudent = new Student("test", "test1234", "1234");
+
+        persistanceLayer.postUser(testStudent);
+
+        testStudent.m_password = "4321";
+
+        persistanceLayer.putUser(testStudent);
+        Student user = persistanceLayer.getUser(testStudent);
+        assertEquals(user.m_cip, "test1234");
+        assertEquals(user.m_name, "test");
+        assertEquals(user.m_password, "4321");
     }
 
     @Test
     void delUserFromJsonFile() {
         PersistanceLayer persistanceLayer = new PersistanceLayer();
-        persistanceLayer.postUser("test1234", "p_fname", "p_lname", "p_password");
-        persistanceLayer.delUser("test1234");
-        assertNull(persistanceLayer.getUser("test1234")); 
+        Student testStudent = new Student("test", "test1234", "1234");
 
+        persistanceLayer.postUser(testStudent);
+        persistanceLayer.delUser(testStudent);
+        assertNull(persistanceLayer.getUser(testStudent)); 
     }
 }
